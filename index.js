7985ede6eb4c4d02dff27e6a0f7bdc0983fda3fc
Legit_Coder <81012612+RajnishPuri@users.noreply.github.com>
2024-10-13 23:31:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://frontend-to-do-seven.vercel.app/',
     credentials: true,
 };
 
